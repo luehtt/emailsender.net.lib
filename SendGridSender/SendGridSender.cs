1c77d6b1e7f1ae02334a219036bd761e5193ce45
Toan Huynh <toan.huynh@beesightsoft.com>
2021-06-29 11:29:45 +0900

    First commit on EmailSender and SendGridSender

---
+++ SendGridSender/SendGridSender.cs
@@ -0,0 +1,82 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EmailSender
+{
+    public class SendGridSender
+    {
+        public string ApiKey { get; private set; }
+        public EmailAddress DefaultEmail { get; private set; }
+
+        public SendGridSender(string apiKey)
+        {
+            ApiKey = apiKey;
+        }
+
+        public SendGridSender(string apiKey, string defaultEmail, string defaultName)
+        {
+            ApiKey = apiKey;
+            DefaultEmail = new EmailAddress(defaultEmail, defaultName);
+        }
+
+        public async Task SendEmailAsync(string fromEmail, string fromName, string toEmail, string toName, string subject, string htmlMessage)
+        {
+            var from = new EmailAddress(fromEmail, fromName);
+            var to = new EmailAddress(toEmail, toName);
+            await SendEmailAsync(from, to, subject, htmlMessage);
+        }
+
+        public async Task SendEmailAsync(string toEmail, string toName, string subject, string htmlMessage)
+        {
+            var to = new EmailAddress(toEmail, toName);
+            await SendEmailAsync(DefaultEmail, to, subject, htmlMessage);
+        }
+
+        public async Task SendEmailAsync(EmailAddress to, string subject, string htmlMessage)
+        {
+            await SendEmailAsync(DefaultEmail, to, subject, htmlMessage);
+        }
+
+        public async Task SendEmailAsync(EmailAddress from, EmailAddress to, string subject, string htmlMessage)
+        {
+            var client = new SendGridClient(ApiKey);
+            var email = MailHelper.CreateSingleEmail(from, to, subject, htmlMessage, htmlMessage);
+            await client.SendEmailAsync(email);
+        }
+
+        public async Task SendGroupEmailAsync(List<string> toEmails, List<string> toNames, string subject, string htmlMessage)
+        {
+            await SendGroupEmailAsync(DefaultEmail, toEmails, toNames, subject, htmlMessage);
+        }
+
+        public async Task SendGroupEmailAsync(EmailAddress from, List<string> toEmails, List<string> toNames, string subject, string htmlMessage)
+        {
+            var tos = CreateEmails(toEmails, toNames);
+            await SendGroupEmailAsync(from, tos, subject, htmlMessage);
+        }
+
+        public async Task SendGroupEmailAsync(List<EmailAddress> tos, string subject, string htmlMessage)
+        {
+            await SendGroupEmailAsync(DefaultEmail, tos, subject, htmlMessage);
+        }
+
+        public async Task SendGroupEmailAsync(EmailAddress from, List<EmailAddress> tos, string subject, string htmlMessage)
+        {
+            var client = new SendGridClient(ApiKey);
+            var email = MailHelper.CreateSingleEmailToMultipleRecipients(from, tos, subject, htmlMessage, htmlMessage);
+            await client.SendEmailAsync(email);
+        }
+
+        private List<EmailAddress> CreateEmails(List<string> toEmails, List<string> toNames)
+        {
+            var res = new List<EmailAddress>();
+            for (var i = 0; i < toEmails.Count; i++)
+            {
+                res.Add(new EmailAddress(toEmails[i], toEmails[i]));
+            }
+            return res;
+        }
+    }
+}
---
+++ StmpEmailSender/StmpEmailSender.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EmailSender
+{
+    public class StmpEmailSender : IEmailSender
+    {
+        public string Host { get; private set; }
+        public int Port { get; private set; }
+        public bool EnableSSL { get; private set; }
+        public string UserName { get; private set; }
+        public string Password { get; private set; }
+
+        public StmpEmailSender(string host, int port, bool enableSSL, string userName, string password)
+        {
+            Host = host;
+            Port = port;
+            EnableSSL = enableSSL;
+            UserName = userName;
+            Password = password;
+        }
+
+        public void SendEmail(string email, string subject, string htmlMessage)
+        {
+            var client = new SmtpClient(Host, Port)
+            {
+                Credentials = new NetworkCredential(UserName, Password),
+                EnableSsl = EnableSSL
+            };
+
+            var message = new MailMessage(UserName, email, subject, htmlMessage) { IsBodyHtml = true };
+            client.Send(message);
+        }
+
+        public void SendGroupEmail(List<string> emails, string subject, string htmlMessage)
+        {
+            var client = new SmtpClient(Host, Port)
+            {
+                Credentials = new NetworkCredential(UserName, Password),
+                EnableSsl = EnableSSL
+            };
+
+            var messages = emails.Select(e => new MailMessage(UserName, e, subject, htmlMessage) { IsBodyHtml = true });
+            foreach (var message in messages)
+            {
+                client.Send(message);
+            }
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var client = new SmtpClient(Host, Port)
+            {
+                Credentials = new NetworkCredential(UserName, Password),
+                EnableSsl = EnableSSL
+            };
+
+            var message = new MailMessage(UserName, email, subject, htmlMessage) { IsBodyHtml = true };
+            await client.SendMailAsync(message);
+        }
+
+        public async Task SendGroupEmailAsync(List<string> emails, string subject, string htmlMessage)
+        {
+            var client = new SmtpClient(Host, Port)
+            {
+                Credentials = new NetworkCredential(UserName, Password),
+                EnableSsl = EnableSSL
+            };
+
+            var messages = emails.Select(e => new MailMessage(UserName, e, subject, htmlMessage) { IsBodyHtml = true });
+            var tasks = messages.Select(e => client.SendMailAsync(e));
+            await Task.WhenAll(tasks);
+        }
+    }
+}
